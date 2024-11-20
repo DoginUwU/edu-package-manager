84c29e7c0ad0c3dfd9c2aff89a07325673e7e2cb
Luiz E. <doginuwu@gmail.com>
2024-11-19 22:34:27 -0300

    fix: find package in github

--- src/commands/install.zig
+++ src/commands/install.zig
@@ -6,13 +6,16 @@ const downloader = @import("../commons/downloader.zig");
 
 pub fn handlerInstall(allocator: std.mem.Allocator, value: []const u8) !bool {
     std.debug.print("Trying to find package with name: {s}\n", .{value});
-    const source = try downloader.readFromURL(allocator, "");
 
-    // comptime var idx = 0;
-    // inline while (idx < packages.len) : (idx += 1) {
-    //     const package = packages[idx];
+    var url = std.ArrayList(u8).init(allocator);
+    defer url.deinit();
 
-    // if (std.mem.eql(u8, value, package.name)) {
+    try url.appendSlice("https://raw.githubusercontent.com/DoginUwU/edu-package-manager/refs/heads/main/packages/");
+    try url.appendSlice(value);
+    try url.appendSlice("/build.edu");
+
+    const source = try downloader.readFromURL(allocator, url.items);
+
     std.debug.print("Package found! Initializing...\n\n", .{});
 
     var context = BuilderContext.init(allocator);
@@ -24,15 +27,14 @@ pub fn handlerInstall(allocator: std.mem.Allocator, value: []const u8) !bool {
     const install_function = context.getFunction("install") orelse return error.MissingInstallFunction;
     const uninstall_function = context.getFunction("uninstall") orelse return error.MissingInstallFunction;
 
-    _ = package_name;
+    if (!std.mem.eql(u8, package_name, value)) {
+        return error.PackageNameConflict;
+    }
+
     _ = package_version;
     _ = uninstall_function;
 
     try install_function.execute(allocator);
 
     return true;
-    //     }
-    // }
-
-    // return false;
 }
